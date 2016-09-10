264d0119cb55068e76c3dd43ea26302d026b8dff
tresiwald <andreas.waldis@live.com>
2016-09-10 19:41:44 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,8 +1,3 @@
-var express = require('express');
-var app = express();
-
-app.use(express.static(__dirname + '/dist'));
-
 var webpack = require('webpack');
 var WebpackDevServer = require('webpack-dev-server');
 var config = require('./webpack.config');
