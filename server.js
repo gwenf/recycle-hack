8014691827eb0d6818359b7837397741b5318d2d
tresiwald <andreas.waldis@live.com>
2016-09-10 19:37:59 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,20 +1,26 @@
-const Server = require('./server.js')
-const port = (process.env.PORT || 3000)
-const app = Server.app()
+var express = require('express');
+var app = express();
 
-if (process.env.NODE_ENV !== 'production') {
-  const webpack = require('webpack')
-  const webpackDevMiddleware = require('webpack-dev-middleware')
-  const webpackHotMiddleware = require('webpack-hot-middleware')
-  const config = require('./webpack.config.js')
-  const compiler = webpack(config)
+app.use(express.static(__dirname + '/dist'));
 
-  app.use(webpackHotMiddleware(compiler))
-  app.use(webpackDevMiddleware(compiler, {
-    noInfo: true,
-    publicPath: config.output.publicPath
-  }))
-}
+var webpack = require('webpack');
+var WebpackDevServer = require('webpack-dev-server');
+var config = require('./webpack.config');
 
-app.listen(port)
-console.log(`Listening at http://localhost:${port}`)
+var server_port = process.env.YOUR_PORT || process.env.PORT || 3000;
+var server_host = process.env.YOUR_HOST || '0.0.0.0';
+
+console.log("Host " + server_host);
+console.log("Port " + server_port);
+
+new WebpackDevServer(webpack(config), {
+  publicPath: config.output.publicPath,
+  hot: true,
+  historyApiFallback: true
+}).listen(server_port, 'localhost', function (err, result) {
+  if (err) {
+    return console.log(err);
+  }
+
+  console.log('Listening at http://localhost:' + port);
+});
