dc522aed67dd0996f4000b56cd47f2177a56a187
gwenf <gwenfaraday@gmail.com>
2016-09-10 19:49:15 -0400

    server

--- server.js
+++ server.js
@@ -1,23 +1,3 @@
-
-
-var server_port = process.env.YOUR_PORT || process.env.PORT || 3000;
-var server_host = process.env.YOUR_HOST || '0.0.0.0';
-
-console.log("Host " + server_host);
-console.log("Port " + server_port);
-
-// new WebpackDevServer(webpack(config), {
-//   publicPath: config.output.publicPath,
-//   hot: true,
-//   historyApiFallback: true
-// }).listen(server_port, 'localhost', function (err, result) {
-//   if (err) {
-//     return console.log(err);
-//   }
-
-//   console.log('Listening at http://localhost:' + port);
-// });
-
 const path = require('path')
 const express = require('express')
 
