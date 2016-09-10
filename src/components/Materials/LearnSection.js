caec683cfce06082dcff702124fa1dedb4b4a51d
gwenf <gwenfaraday@gmail.com>
2016-09-10 16:59:12 -0400

    fixing learn autopoluating

--- src/components/Materials/LearnSection.js
+++ src/components/Materials/LearnSection.js
@@ -2,13 +2,25 @@ import React, { Component } from 'react'
 import { connect } from 'react-redux'
 
 class LearnSection extends Component {
+  constructor(){
+    super();
+    this.state = {
+      material: ''
+    }
+  }
+  componentWillMount(){
+    var material = localStorage.getItem('chosenMaterial');
+    this.setState({
+      material
+    })
+  }
   render() {
   	var that = this;
   	var types = [];
   	var materialsArr = '';
   	// if (this.props.material.length > 0){
   		var material = this.props.materials.filter(function(val){
-  			if (val.name === that.props.material){
+  			if (val.name === that.state.material){
   				val.types.map(function(x){
   					types.push(x.type)
   				})
