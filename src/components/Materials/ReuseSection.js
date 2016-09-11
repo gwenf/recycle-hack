e03e8c7fbb9f60e18561987a51a0e3c6c4d8feda
gwenf <gwenfaraday@gmail.com>
2016-09-11 10:16:23 -0400

    added prod

--- src/components/Materials/ReuseSection.js
+++ src/components/Materials/ReuseSection.js
@@ -18,6 +18,8 @@ class ReuseSection extends Component {
     return (
       <div className='sections'>
         <h1>Ideas for Reusing {this.state.material.replace(this.state.material[0], this.state.material[0].toUpperCase())}</h1>
+        <div className='social-media-hashtag'></div>
+        <div className='social-media-hashtag'></div>
         <ul>
           <li>List of ways to reuse {this.state.material}...</li>
         </ul>
