ffb0b6a6c77913cff73ebdad17b19404307df635
gwenf <gwenfaraday@gmail.com>
2016-09-10 21:20:07 -0400

    fixing server.js

--- server.js
+++ server.js
@@ -1,25 +1,3 @@
-var webpack = require('webpack');
-var WebpackDevServer = require('webpack-dev-server');
-var config = require('./webpack.config');
-
-var server_port = process.env.YOUR_PORT || process.env.PORT || 3000;
-var server_host = process.env.YOUR_HOST || '0.0.0.0';
-
-console.log("Host " + server_host);
-console.log("Port " + server_port);
-
-// new WebpackDevServer(webpack(config), {
-//   publicPath: config.output.publicPath,
-//   hot: true,
-//   historyApiFallback: true
-// }).listen(server_port, 'localhost', function (err, result) {
-//   if (err) {
-//     return console.log(err);
-//   }
-
-//   console.log('Listening at http://localhost:' + port);
-// });
-
 const path = require('path')
 const express = require('express')
 
