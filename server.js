39388ddfd432012b1c1c42ca9468a803dbd8d8de
tresiwald <andreas.waldis@live.com>
2016-09-10 20:49:32 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,36 +1,30 @@
-const path = require('path')
-const express = require('express')
+var express = require('express');
 
-const Server = {
-  app: function () {
-    const app = express()
-    const indexPath = path.join(__dirname, './index.html')
-    const publicPath = express.static(path.join(__dirname, './'))
-
-    app.use( express.static(path.join(__dirname, './')))
-    app.get('/', function (_, res) { res.sendFile(indexPath) })
-
-    return app
-  }
-}
+var path = require('path');
 
 
-const port = (process.env.PORT || 3000)
-const app = Server.app()
-
-if (process.env.NODE_ENV !== 'production') {
-  const webpack = require('webpack')
+     const webpack = require('webpack')
   const webpackDevMiddleware = require('webpack-dev-middleware')
   const webpackHotMiddleware = require('webpack-hot-middleware')
   const config = require('./webpack.config.js')
   const compiler = webpack(config)
+
+app = express();
+
+app.configure(function(){
+    app.use(express.bodyParser());
+    app.use(express.static(path.join(__dirname, 'dist')));
 
   app.use(webpackHotMiddleware(compiler))
   app.use(webpackDevMiddleware(compiler, {
     noInfo: true,
     publicPath: config.output.publicPath
   }))
-}
+});
 
-app.listen(port)
-console.log(`Listening at http://localhost:${port}`)
+var port = process.env.PORT || 3000;
+
+app.listen(port);
+
+console.log('server started '+port);
+
